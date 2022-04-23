6a19c7eff0dd48204a9bcacabbc62c201b87116c
SodaCris <wang-jiaxun@outlook.com>
2022-04-23 11:08:27 +0800

    use coreapp first

--- Scripts/config.ps1
+++ Scripts/config.ps1
@@ -5,4 +5,4 @@ $Env:Qt6GuiTools_DIR = "$PSScriptRoot/../Qt/6.3.0/mingw_64"
 
 $Env:CC="gcc"
 $Env:CXX="g++"
-cmake -B $PSScriptRoot/../build -G "Ninja" -Wno-dev -DDEBUG=1
+cmake -B $PSScriptRoot/../build -G "Ninja" -Wno-dev -DDEBUG=1 -DCMAKE_BUILD_TYPE=Debug
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <QTimer>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app( argc, argv );
+
+    // do your thing, once
+    cout << "Hello world\n";
+
+    QTimer::singleShot( 0, &app, &QCoreApplication::quit );
+    return app.exec();
+}
